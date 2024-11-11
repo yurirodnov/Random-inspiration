b5b5aa01f449e66148a4f0f16877996ba5ed4d7b
yuri_rodnov <somewaystodo@yandex.ru>
2024-11-12 01:09:18 +0300

    test hello world

---
+++ src/lib/Hello.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const hello: string = "Hello, world!"
+</script>
+
+
+<div class="hello-block">
+  <p class="hello-paragraph">{hello}</p>
+</div>
+
+<style>
+  .hello-block {
+    background-color: brown;
+  }
+</style>
