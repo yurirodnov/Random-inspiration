a798388025897e7e276635ccd8956aadaedf6f8f
yuri_rodnov <somewaystodo@yandex.ru>
2024-11-12 15:32:10 +0300

    Test axios

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">  
   import Hello from './lib/Hello.svelte'
+  import GetQuotes from './lib/api/GetQuotes.svelte';
 </script>
 
 <main>
   <Hello />
+  <GetQuotes />
 </main>
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/random': {
+        target: 'https://zenquotes.io/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/quote/, ''),
+      },
+    },
+  },
 })
